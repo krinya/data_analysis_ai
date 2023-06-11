58b5675714562c42170ffb0ff712cdab08de704a
Juhi <juchic2811@gmail.com>
2023-06-11 19:15:33 +0200

    context commit new

--- utils/da_functions.py
+++ utils/da_functions.py
@@ -39,10 +39,9 @@ def generate_answer_using_context(query, context=None, conversation=None):
 
     return answer_message['content'], conversation
 
-
 def get_response_from_chat_gpt(input, context=None):
     # call the chatbot and get the response
-    if context!=None:
+    if context:
         context_in=context
     else:
         context_in=""
